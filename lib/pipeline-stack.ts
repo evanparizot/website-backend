ce1403fb2decfc45f99d13f1b09d0cd6c83dc76b
evan <eparizot@gmail.com>
2020-11-26 11:37:27 -0600

    Add maven build command

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -5,6 +5,7 @@ import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import { WebsiteStage } from './website-stage';
 
 //https://aws.amazon.com/blogs/developer/cdk-pipelines-continuous-delivery-for-aws-cdk-applications/
+//https://docs.aws.amazon.com/cdk/api/latest/docs/pipelines-readme.html
 
 export class WebsitePipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -25,15 +26,22 @@ export class WebsitePipelineStack extends Stack {
                 repo: 'website-backend'
             }),
 
-            synthAction: SimpleSynthAction.standardNpmSynth({
+            synthAction: new SimpleSynthAction({
                 sourceArtifact,
                 cloudAssemblyArtifact,
-                buildCommand: 'npm run build',
+                installCommands: ['npm install -g aws-cdk'],
+                buildCommands: ['cd src && mvn package'],
+                synthCommand: 'cdk synth'
             })
+            // synthAction: SimpleSynthAction.standardNpmSynth({
+            //     sourceArtifact,
+            //     cloudAssemblyArtifact,
+                
+            // })
         });
 
         // Define application stages here
-        pipeline.addApplicationStage(new WebsiteStage(this, 'Staging', {
+        const stagingStage = pipeline.addApplicationStage(new WebsiteStage(this, 'Staging', {
             env: { 
                 account: '591024261921', 
                 region: 'us-east-2'
@@ -41,7 +49,9 @@ export class WebsitePipelineStack extends Stack {
             apiUrl: "api.staging.evanparizot.com"
         }));
 
-        // pipeline.addApplicationStage(new WebsiteStage(this, 'Production', {
+        // stagingStage.addActions(new Shell)
+
+        // const productionStage = pipeline.addApplicationStage(new WebsiteStage(this, 'Production', {
         //     env: {
         //         account: '',
         //         region: 'us-east-2'
--- src/src/main/java/com/evanparizot/projects/ProjectsHandler.java
+++ src/src/main/java/com/evanparizot/projects/ProjectsHandler.java
@@ -8,10 +8,9 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.Map;
 
 public class ProjectsHandler implements RequestHandler<Map<String, String>, String> {
-
-
     public String handleRequest(Map<String, String> stringStringMap, Context context) {
         LambdaLogger logger = context.getLogger();
+        logger.log("Hello there");
         return null;
     }
 }
