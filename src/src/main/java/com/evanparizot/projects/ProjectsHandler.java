df2ad84f216e26cc7186f56903bebda17dbfbf72
evan <eparizot@gmail.com>
2020-11-26 15:16:41 -0600

    Use strongly type lambda proxy event

--- src/src/main/java/com/evanparizot/projects/ProjectsHandler.java
+++ src/src/main/java/com/evanparizot/projects/ProjectsHandler.java
@@ -4,13 +4,15 @@ package com.evanparizot.projects;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-import java.util.Map;
+public class ProjectsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
 
-public class ProjectsHandler implements RequestHandler<Map<String, String>, String> {
-    public String handleRequest(Map<String, String> stringStringMap, Context context) {
         LambdaLogger logger = context.getLogger();
         logger.log("Hello there");
+        logger.log(String.valueOf(event));
         return null;
     }
 }
