33dbe00e338a1f3be73d873223dc6203afe067e7
evan <eparizot@gmail.com>
2021-02-20 23:10:55 -0600

    Fix unit tests

--- src/WebsiteLambda.Tests/ProjectsControllerTests.cs
+++ src/WebsiteLambda.Tests/ProjectsControllerTests.cs
@@ -33,7 +33,7 @@ namespace WebsiteLambda.Tests
         {
             var input = new CreateProjectRequest
             {
-                Content = "Test"
+                ContentBody = "Test"
             };
 
             var result = await _toTest.CreateProjectAsync(input);
